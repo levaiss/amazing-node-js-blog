0b1d30bb8c16fe69eb6f569edf3233b9d0ef7e2d
Dmytro <weblevin@gmail.com>
2024-02-12 12:51:53 +0200

    Clear src folder

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,8 @@
 import 'dotenv/config';
-import path from 'path';
 import express, { Application } from 'express';
-import { DbService } from './services/DbService';
 import router from './router/index';
-import { srcPath } from './utils/path-helper';
-import { ensureDirectoryExists } from './utils/file-helper';
 
 const port = process.env.PORT || 8000;
-
-(async () => {
-  const dbFolderPath = path.join(srcPath, 'db');
-  await ensureDirectoryExists(dbFolderPath);
-  const db = DbService.getInstance();
-  await db.create(dbFolderPath, 'db.json', {
-    users: [
-      {
-        login: "admin",
-        password: "1234"
-      }
-    ]
-  });
-})()
 
 const app: Application = express();
 
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,9 @@
 import { Request, Response, Router } from 'express';
 
-import apiRouter from './api/index';
-
 const router = Router();
 
 router.get('/', (req: Request, res: Response) => {
   res.send('⚡️Welcome to Express Server!');
 });
-
-router.use('/api', apiRouter);
 
 export default router;
