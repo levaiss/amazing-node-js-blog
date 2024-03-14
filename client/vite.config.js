d8b80d0e7464fb8a2043a0c72765fc959d951cb2
Dmytro <weblevin@gmail.com>
2024-03-14 10:39:08 +0200

    Update env settings

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,19 +1,27 @@
+import dotenv from 'dotenv'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+dotenv.config({ path: '../.env' })
+
 // eslint-disable-next-line no-undef
-const isDev = process.env.APP_ENV === 'development'
+const APP_ENV = process.env.VITE_APP_ENV;
 // eslint-disable-next-line no-undef
-const PORT = process.env.APP_PORT || 3000;
+const API_HOST = process.env.VITE_API_HOST;
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    ...isDev && {
-      proxy: {
-        '/api': `http://localhost:${PORT}`
+export default defineConfig(() => {
+  const isDev = APP_ENV === 'development';
+
+  return {
+    envDir: '../',
+    server: {
+      ...isDev && {
+        proxy: {
+          '/api': API_HOST
+        }
       }
-    }
-  },
-  plugins: [react()],
+    },
+    plugins: [react()],
+  }
 })
