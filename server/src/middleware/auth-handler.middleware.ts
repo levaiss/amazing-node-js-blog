1bc3263a34f562ed8cd575b520c2a814581b9f9c
Dmytro <weblevin@gmail.com>
2024-03-17 20:51:01 +0200

    Update auth middleware, add common error format

--- server/src/middleware/auth-handler.middleware.ts
+++ server/src/middleware/auth-handler.middleware.ts
@@ -1,6 +1,24 @@
+// Core
+import { NextFunction, Request, Response } from 'express';
 import passport from 'passport';
+
+// Services
+import { IUserModel } from '../service/database/model/user.model';
 import { AUTH_STRATEGIES_TYPE, AuthStrategiesType } from '../service/auth';
 
+// Helpers
+import { UnauthorizedError } from '../errors';
+
 export function authHandlerMiddleware(strategyName: AuthStrategiesType = AUTH_STRATEGIES_TYPE.ACCESS_TOKEN) {
-  return passport.authenticate(strategyName, { session: false });
+  return (req: Request, res: Response, next: NextFunction) => {
+    passport.authenticate(strategyName, (error: Error, user: IUserModel) => {
+      if (error) {
+        return next(error);
+      }
+
+      if (!user) {
+        return next(new UnauthorizedError());
+      }
+    })(req, res, next);
+  };
 }
